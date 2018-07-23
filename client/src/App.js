7b3569ea987832a1ad13c4d45f3c2454ed91d937
= <=>
2018-07-23 17:02:22 +0500

    Second Edition

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Login2 from "./components/auth/Login2";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login2" component={Login2} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
